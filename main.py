104ec77ecf6ac699193f0ba27b4151fbe05476aa
Naaga <naaga@Naagas-MacBook-Pro.local>
2019-06-21 12:16:56 -0500

    im a message about this commit

--- main.py
+++ main.py
@@ -1,57 +1,49 @@
 from flask import Flask, request
 from caesar import rotate_string
-import format
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
 form = """
-
 <!DOCTYPE html>
-
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
-           
+            }}
         </style>
     </head>
     <body>
-      <html>
-    <body>
-        <form action = "/encrypt" method="post">
-            <label for= "rotate by">Rorate by:</label>
-            <input type="text" name="Rotate_by" value="0"/>
-            <br><br>
-            <textarea rows="10" cols="30"></textarea>
-            <br><br>
-            <input type= "Submit" value= "Submit Query"/>
+        <form method='POST'>
+            <label>Rotate by:</label>
+            <input type="text" name="rot" value="0" />
+            <textarea name="text">{0}</textarea>
+            <input type="submit" value="Submit Query" />            
         </form>
-    </body>
-</html>
+    
     </body>
 </html>
 """
+
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
-@app.route("/encrypt", methods=['POST'])   
-def Rotate_string(): 
-    encrypt = request.form['Rotate_by']
-    return '<h1>Rotate_string, '+ encrypt + '<h1>'
+@app.route("/", methods=['POST'])
+def encrypt():
+    rotate_number = int(request.form['rot'])
+    message = request.form['text']
+    return form.format(rotate_string(message, rotate_number))
+
 app.run()
